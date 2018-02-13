9f2315bf3f943598045e857c97ad3ece4f99c577
Vijay Chintalapati <Vijay.Chintalapati@gmail.com>
2018-02-12 21:14:24 -0800

    Added archetype for a simple JDG server client using Hotrod protocol

---
+++ infinispan-server-client-archetype/src/main/resources/archetype-resources/src/main/java/JDGRemoteClientConsoleApp.java
@@ -0,0 +1,35 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import java.io.IOException;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.Configuration;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JDGRemoteClientConsoleApp {
+
+    private static final Logger logger = LoggerFactory.getLogger(JDGRemoteClientConsoleApp.class);
+
+    public static void main(String[] args) {
+        Configuration configuration = new ConfigurationBuilder().build();
+        RemoteCacheManager cacheManager = new RemoteCacheManager(configuration);
+
+        // Get a cache instance using cacheManager
+        RemoteCache<String, Integer> remoteCache = cacheManager.getCache();
+
+        // Perform some cache operations
+        remoteCache.put("1", 1);
+
+        // Log any stats onto the console
+        logger.info("The size of the cache : {}",remoteCache.size());
+
+        // Stop the cache manager
+        cacheManager.stop();
+    }
+}
